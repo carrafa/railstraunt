b20c819f3b55c378e560d26de5db6725e8904b8b
Joshua Carrafa <carrafa@gmail.com>
2015-12-17 18:54:45 -0500

    fixes party view

--- app/models/party.rb
+++ app/models/party.rb
@@ -5,11 +5,11 @@ class Party < ActiveRecord::Base
 
   def populate_seats num
     seats = {
-      "A" => ["A1", "A2", "A3", "A4"],
-      "B" => ["B1", "B2", "B3"],
-      "C" => ["C1", "C2", "C3", "C4"],
+      "A" => ["A1", "A2"],
+      "B" => ["B1", "B2"],
+      "C" => ["C1", "C2", "C3", "C4", "C6", "C8"],
       "D" => ["D1", "D2", "D3", "D4"],
-      "Bar" => ["Bar1", "Bar2", "Bar3", "Bar4"]
+      "E" => ["E1", "E2", "E3", "E4"],
     }
     return seats[num]
   end
